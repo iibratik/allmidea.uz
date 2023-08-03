275f0b3ce37e8551a0befd12162d1e4adea8a62a
iibratik <iibragimov2417@gamil.com>
2023-08-03 23:21:00 +0500

    some ref

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,9 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-  state: {
-
-  },
-  getters: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  },
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
   modules: {},
 })
