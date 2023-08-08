51c892f3c702155c21b546697c94d300cd1f1f59
iibratik <iibragimov2417@gamil.com>
2023-08-08 20:12:11 +0500

    adding loader

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,33 @@
 <template>
   <v-app>
-      <Navbar />
-      <router-view></router-view>
+    <Navbar />
+    <Loader v-if="loaderStatus">
+    <router-view>
+    </router-view>
+    </Loader>
+    <router-view v-else>
+    </router-view>
+
     <Footer />
   </v-app>
 </template>
+
 <script>
-import Footer from '@/components/Footer.vue'
-import Navbar from '@/components/Navbar.vue'
+import Footer from '@/components/Footer.vue';
+import Navbar from '@/components/Navbar.vue';
+import Loader from './components/Loader.vue';
+
 export default {
-  components: { Navbar, Footer },
-}
+  components: { Navbar, Footer, Loader },
+  data() {
+    return {
+      loaderStatus: true,
+    };
+  },
+  created() {
+    setTimeout(() => {
+      this.loaderStatus = false; // Установите loaderStatus в false после 5 секунд
+    }, 5000);
+  },
+};
 </script>
---
+++ src/components/Loader.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="loader">
+    <div class="spinner" :style="{ borderColor: color }">
+      <div class="inner-circle"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      default: '#4CAF50'
+    }
+  }
+};
+</script>
+
+<style scoped>
+.loader {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+.spinner {
+  border: 15px solid rgba(0, 0, 0, 0.1);
+  border-left-color: transparent;
+  border-radius: 45%;
+  width: 120px;
+  height: 120px;
+  animation: spin 1s linear infinite;
+  position: relative; /* Добавим позиционирование для вложенного круга */
+}
+
+.inner-circle {
+  width: 60px;
+  height: 60px;
+  background-color: #0590D7;
+  border-radius: 45%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+@keyframes spin {
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
